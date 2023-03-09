023200c2dda06160c6517f82b43d0d3a44d60fc6
Gyulguma <ybg653924@gmail.com>
2023-03-09 18:49:39 +0900

    YBG [3] [PGS] 완주하지 못한 선수

--- week1/YooByeong_gue/Main3.java
+++ week1/YooByeong_gue/Main3.java
@@ -9,17 +9,39 @@ public class Main3 {
     }
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
+        // 1. 참가자 명단에서 각 동명이인이 몇명인지 체크한다.
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i=0;i<participant.length;i++){
+            map.put(participant[i], map.getOrDefault(participant[i],0)+1);
+        }
+        // 2. 완주자 명단에 이름이 있다면 해당 이름의 명 수를 감소한다.
+        for(String key : completion){
+            map.put(key, map.get(key) -1);
+        }
+        // 3. 참가자 명단에서 아직 동명이인의 명 수가 0이 아닌 사람을 찾는다. = answer
+        for(String key : map.keySet()){
+            if(map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+        return answer;
+    }
+}
+/*  정확성 테스트 통과 / 효율성 테스트 실패 (시간초과)
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
         HashMap<Integer, String> map = new HashMap<>();
         for(int i=0; i<completion.length;i++)
             map.put(i,completion[i]);
         for(String name : participant){
-            if(!map(name)){
+            if(!map.containsValue(name)){
                 answer = name;
                 break;
             }
-               map.values().remove(name);
+            map.values().remove(name);
         }
-        System.out.println(map.values().toString());
         return answer;
     }
-}
+
+ */
