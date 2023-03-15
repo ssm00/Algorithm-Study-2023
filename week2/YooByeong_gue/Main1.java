133035d940bdf45e20fc80309b8f5f7d971fa69d
Gyulguma <ybg653924@gmail.com>
2023-03-16 00:33:10 +0900

    YBG [1] [PGS] 같은 숫자는 싫어

--- week2/YooByeong_gue/Main1.java
+++ week2/YooByeong_gue/Main1.java
@@ -1,4 +1,20 @@
 package week2.YooByeong_gue;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Main1 {
+
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(arr[0]);
+        for(int i=1; i<arr.length;i++){
+            if(temp.get(temp.size()-1) != arr[i]){
+                temp.add(arr[i]);
+            }
+        }
+        answer = temp.stream().mapToInt(Integer::intValue).toArray();
+        return answer;
+    }
 }
