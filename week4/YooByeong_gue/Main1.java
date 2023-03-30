d24bdae1b59544d8a72787a033083c7ef40f893d
Gyulguma <ybg653924@gmail.com>
2023-03-31 00:29:36 +0900

    YBG [1] [PGS] K번째수

--- week4/YooByeong_gue/Main1.java
+++ week4/YooByeong_gue/Main1.java
@@ -1,4 +1,24 @@
 package week4.YooByeong_gue;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main1 {
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = {};
+        List<Integer> result = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        for(int i=0; i< commands.length; i++){
+            for(int j=commands[i][0]-1;j<commands[i][1];j++){
+                temp.add(array[j]);
+            }
+            Collections.sort(temp);
+            result.add(temp.get(commands[i][2]-1));
+            temp.clear();
+        }
+
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
